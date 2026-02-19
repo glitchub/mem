07d34d94c7596dbeb6d0b45c652d083c533caab3
glitchub <glitchub@nowhere>
2026-02-19 10:34:34 -0800

    Support byte-swapped

--- mem.c
+++ mem.c
@@ -3,6 +3,7 @@
 
 // See https://github.com/glitchub/mem for more information.
 
+#include <byteswap.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <inttypes.h>
@@ -19,7 +20,7 @@ static void usage(void)
 {
     die(R"(Usage:
 
-    mem [width] operation [... [width] operation]
+    mem [mode] operation [... [mode] operation]
 
 Perform read and write operations on physical memory.
 
@@ -33,15 +34,18 @@ Perform read and write operations on physical memory.
 
 Addresses and values can be up to 64-bit, given in decimal, hex, or octal.
 
-"width" can be one of the following, and sets the data size of all subsequent operations
-(until the next width character):
+"mode" can be one of the following, and sets the bit width and relative endian-ness of all subsequent
+operations (until the next mode character):
 
-   b - 8 bit
-   h - 16 bit
-   w - 32 bit (this is the default)
-   d - 64 bit
+   b - 8-bit
+   h - native 16-bit
+   w - native 32-bit (this is the default)
+   d - native 64-bit
+   H - swapped 16-bit
+   W - swapped 32-bit
+   D - swapped 64-bit
 
-Values that exceed the current width are silently truncated.
+Input values that exceed the current width are silently truncated.
 
 Output values are printed to stdout in hex, zero-justified to the current width, one per line.
 
@@ -69,26 +73,31 @@ int main(int argc, char* argv[])
     {
         int operator; // one of the enums above
         int width;
+        int swap;
         uint64_t address;
         uint64_t data;
     } OP[MAXOPS] = {0};
 
-    int width = 32, ops = 0;
+    int width = 32, swap = 0, ops = 0;
 
-    while (optind < argc)
+    for (int x = 1; x < argc; x++)
     {
-        char *arg = argv[optind++], *p;
+        char *arg = argv[x], *p;
         switch (*arg)
         {
-            case 'b': width = 8; continue;
-            case 'h': width = 16; continue;
-            case 'w': width = 32; continue;
-            case 'd': width = 64; continue;
+            case 'b': width =  8; swap = 0; continue;
+            case 'h': width = 16; swap = 0; continue;
+            case 'w': width = 32; swap = 0; continue;
+            case 'd': width = 64; swap = 0; continue;
+            case 'H': width = 16; swap = 1; continue;
+            case 'W': width = 32; swap = 1; continue;
+            case 'D': width = 64; swap = 1; continue;
         }
 
         if (ops == MAXOPS) die ("Too many operations\n");
 
         OP[ops].width = width;
+        OP[ops].swap = swap;
         OP[ops].address = strtoull(arg, &p, 0);
         if (p == arg) goto choke;
         switch (*p++)
@@ -122,9 +131,15 @@ int main(int argc, char* argv[])
 
         // get value
         if (!*p) goto choke;
-        OP[ops].data = strtoull(p, &p, 0);
+        uint64_t data = strtoull(p, &p, 0);
         if (*p) goto choke;
-
+        if (swap) switch(width)
+        {
+            case 16: data = bswap_16(data); break;
+            case 32: data = bswap_32(data); break;
+            case 64: data = bswap_64(data); break;
+        }
+        ops[OP].data = data;
         next: ops++; // next
     }
 
@@ -136,9 +151,9 @@ int main(int argc, char* argv[])
 
     for (int op = 0; op < ops; op++)
     {
-        #define PAGESIZE 4096 // XXX get this from sysconf?
-        int offset = OP[op].address % PAGESIZE;
-        int size = ((offset + (OP[op].width/8)) > PAGESIZE) ? 2*PAGESIZE : PAGESIZE;
+        int pagesize = getpagesize();
+        int offset = OP[op].address % pagesize;
+        int size = ((offset + (OP[op].width/8)) > pagesize) ? 2*pagesize : pagesize;
         void *map = mmap(NULL, size, PROT_READ|PROT_WRITE, MAP_SHARED, fd, (off_t)(OP[op].address - offset));
         if (map == MAP_FAILED) die("Can't map address 0x%" PRIX64" :%s\n", OP[op].address, strerror(errno));
         void *address = map + offset;
@@ -148,10 +163,30 @@ int main(int argc, char* argv[])
             case READ:
                 switch(OP[op].width)
                 {
-                    case  8: printf("0x%.2"   PRIX8 "\n", *(volatile uint8_t *)  address); break;
-                    case 16: printf("0x%.4"  PRIX16 "\n", *(volatile uint16_t *) address); break;
-                    case 32: printf("0x%.8"  PRIX32 "\n", *(volatile uint32_t *) address); break;
-                    case 64: printf("0x%.16" PRIX64 "\n", *(volatile uint64_t *) address); break;
+                    case  8:
+                        printf("0x%.2" PRIX8 "\n", *(volatile uint8_t *) address);
+                        break;
+                    case 16:
+                    {
+                        uint16_t data = *(volatile uint16_t *) address;
+                        if (OP[op].swap) data = bswap_16(data);
+                        printf("0x%.4" PRIX16 "\n", data);
+                        break;
+                    }
+                    case 32:
+                    {
+                        uint32_t data = *(volatile uint32_t *) address;
+                        if (OP[op].swap) data = bswap_32(data);
+                        printf("0x%.8" PRIX32 "\n", data);
+                        break;
+                    }
+                    case 64:
+                    {
+                        uint64_t data = *(volatile uint64_t *) address;
+                        if (OP[op].swap) data = bswap_64(data);
+                        printf("0x%.16" PRIX64 "\n", data);
+                        break;
+                    }
                 }
                 break;
 
